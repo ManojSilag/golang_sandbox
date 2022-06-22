94c56437b5a6788b6e26b4d16955be6a0b775e05
HP <manoj.silag@hipaas.com>
2022-06-22 13:21:23 +0530

    Adding changes

--- demo/arrays/arrays.go
+++ demo/arrays/arrays.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type Room struct {
+	name    string
+	cleaned bool
+}
+
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is cleaned")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
 func main() {
+
+	rooms := [...]Room{
+		{name: "office", cleaned: false},
+		{name: "Warehouse", cleaned: false},
+		{name: "Reception", cleaned: false},
+		{name: "Ops", cleaned: false},
+	}
+	checkCleanliness(rooms)
+
+	fmt.Println("Performing cleaning......")
+
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+
+	checkCleanliness(rooms)
 
 }
--- exercise/arrays/arrays.go
+++ exercise/arrays/arrays.go
@@ -18,6 +18,43 @@ package main
 
 import "fmt"
 
+type product struct {
+	name  string
+	price int
+}
+
+func statistics(itemss [4]product) {
+
+	var lastitem product
+	var totalitem int
+	var totalcost int
+	for i := 0; i < len(itemss); i++ {
+		// fmt.Println(itemss[i])
+		item := itemss[i]
+		totalcost += item.price
+		if item.name != "" && item.price != 0 {
+			totalitem += 1
+			lastitem = item
+		}
+	}
+	fmt.Println("Total cost", totalcost)
+	fmt.Println("Total items", totalitem)
+	fmt.Println("Lastitem", lastitem)
+
+}
+
 func main() {
+
+	items := [4]product{
+		{name: "choclate", price: 10},
+		{name: "Ice cream", price: 25},
+		{name: "waffels", price: 180},
+	}
+
+	statistics(items)
+
+	items[3] = product{name: "food", price: 400}
+
+	statistics(items)
 
 }
