0a5268edce7561e3ce5ae18aed3a0b812f938bf0
HP <manoj.silag@hipaas.com>
2022-07-02 12:27:33 +0530

    Reader and Writer Buffer

--- exercise/reader/reader.go
+++ exercise/reader/reader.go
@@ -1,25 +1,80 @@
-//--Summary:
-//  Create an interactive command line application that supports arbitrary
-//  commands. When the user enters a command, the program will respond
-//  with a message. The program should keep track of how many commands
-//  have been ran, and how many lines of text was entered by the user.
-//
-//--Requirements:
-//* When the user enters either "hello" or "bye", the program
-//  should respond with a message after pressing the enter key.
-//* Whenever the user types a "Q" or "q", the program should exit.
-//* Upon program exit, some usage statistics should be printed
-//  ('Q' and 'q' do not count towards these statistics):
-//  - The number of non-blank lines entered
-//  - The number of commands entered
-//
-//--Notes
-//* Use any Reader implementation from the stdlib to implement the program
+// //--Summary:
+// //  Create an interactive command line application that supports arbitrary
+// //  commands. When the user enters a command, the program will respond
+// //  with a message. The program should keep track of how many commands
+// //  have been ran, and how many lines of text was entered by the user.
+// //
+// //--Requirements:
+// //* When the user enters either "hello" or "bye", the program
+// //  should respond with a message after pressing the enter key.
+// //* Whenever the user types a "Q" or "q", the program should exit.
+// //* Upon program exit, some usage statistics should be printed
+// //  ('Q' and 'q' do not count towards these statistics):
+// //  - The number of non-blank lines entered
+// //  - The number of commands entered
+// //
+// //--Notes
+// //* Use any Reader implementation from the stdlib to implement the program
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// func main() {
+// 	r := bufio.NewReader(os.Stdin)
+// 	var numberOfCommands int
+// 	var numberOfLines int
+// 	for {
+// 		input, inputErr := r.ReadString(' ')
+// 		i := strings.TrimSpace(input)
+// 		if i == "" {
+// 			continue
+// 		}
+// 		if strings.ToLower(i) == "q" {
+// 			break
+// 		}
+// 		numberOfCommands += 1
+// 		numberOfLines += 1
+// 		fmt.Println("Hi")
+// 		fmt.Println(inputErr)
+// 	}
+// 	fmt.Printf("numberOfCommands: %v \n", numberOfCommands)
+// 	fmt.Printf("numberOfLines: %v \n", numberOfLines)
+// }
+
+const (
+	cmdHello   = "hello"
+	cmdGoodbye = "bye"
+)
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	numLines := 0
+	numCommands := 0
+	for scanner.Scan() {
+		if strings.ToLower(scanner.Text()) == "q" {
+			break
+		} else {
+			text := strings.TrimSpace(scanner.Text())
+			switch text {
+			case cmdHello:
+				numCommands += 1
+				fmt.Println("command response: hi")
+			case cmdGoodbye:
+				numCommands += 1
+				fmt.Println("command response: bye")
+			}
+			if text != "" {
+				numLines += 1
+			}
+		}
+	}
+	fmt.Printf("You entered %v lines\n", numLines)
+	fmt.Printf("You entered %v commands\n", numCommands)
 
 }
